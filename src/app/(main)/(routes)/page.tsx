5615a3b624a2f1f8d22dfc5ba555cf8f280d548e
Deshraj Tiwari <deshrajtiwari690@gmail.com>
2024-03-10 09:43:01 +0530

    Cleanup and ShadCN initialization.

---
+++ src/app/(main)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex justify-center items-center flex-col">
+      <p>Hello Dashcord!</p>
+      <Button>Shad Cn</Button>
+    </div>
+  );
+}
